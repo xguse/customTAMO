f7b7dc27bbd2a54c50a9bdfb727617c1de70d5f0
biggus <biggus@6f4884b1-7b33-4f67-8431-4e2787ef5163>
2009-10-12 22:54:28 +0000

    changed local paths
    git-svn-id: file:///Users/biggus/Documents/Programming/WingProjects/gTAMO_svn@6 6f4884b1-7b33-4f67-8431-4e2787ef5163

--- localpaths.py
+++ localpaths.py
@@ -1,2 +1,2 @@
 TAMOroot = '/Library/Frameworks/Python.framework/Versions/2.5/lib/python2.5/site-packages/TAMO/'
-TAMOdata = '/Users/biggus/Documents/James/Data/TAMOsupportData /'
+TAMOdata = '/Users/biggus/SourceCodeEtc/TAMO_ChangedToHaveGusMods/TAMO-1.0/'
